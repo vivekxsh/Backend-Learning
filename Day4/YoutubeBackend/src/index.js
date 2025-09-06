fcb8a45e153a4f0787177293ce1afa50ca67b032
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2025-09-06 22:02:41 +0530

    Add User and Video model

--- Day4/YoutubeBackend/src/index.js
+++ Day4/YoutubeBackend/src/index.js
@@ -2,8 +2,11 @@
 
 import dotenv from "dotenv";
 import connectDB from './db/index.js';
+import express from "express";
 
 dotenv.config({path: './env'});
+
+const app = express();
 
 const port = process.env.PORT || 8000;
 
